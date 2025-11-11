06b8ae46401ab5f55e2a087640aff6fcbac35339
Dayne Linke <Daynelinke@gmail.com>
2025-11-11 11:53:44 +1000

    Fixed type command echo problem

--- app/main.py
+++ app/main.py
@@ -8,15 +8,17 @@ def main():
     while (1):
         sys.stdout.write("$ ")
         command = input()
-        if command == "exit 0":
-            exit(0)
-        if "echo" in command:
-            print(f"{command.removeprefix("echo ")}")
         if "type" in command:
             if command.removeprefix("type ") in commands:
                 print(f"{command.removeprefix("type ")} is a shell builtin")
+                continue
             else:
                 print(f"{command.removeprefix("type ")}: not found")
+                continue
+        if command == "exit 0":
+            exit(0)
+        if "echo " in command:
+            print(f"{command.removeprefix("echo ")}")
 
         else:
             print(f"{command}: command not found")
