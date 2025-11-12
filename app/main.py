7eb50585b53739e6073845fcafe5dcfb8cb19494
Dayne Linke <Daynelinke@gmail.com>
2025-11-12 11:12:08 +1000

    Added Path support

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 import sys
+import os
 
-commands = ["exit", "echo", "type"]
+commands = ["exit", "echo", "type", "path"]
+
+
+def checkPath(paths, command):
+    for path in paths:
+        if command in path:
+            pathOfCommand = path
+            return True, pathOfCommand
+        else:
+            return False, ''
 
 
 def main():
@@ -8,20 +18,36 @@ def main():
     while (1):
         sys.stdout.write("$ ")
         command = input()
+        pathseperator = os.pathsep
+        paths = []
+        foundInPath = False
+        pathOfCommand = ''
+
+        paths += os.getenv('PATH').split(pathseperator)
+
         if "type" in command:
-            if command.removeprefix("type ") in commands:
-                print(f"{command.removeprefix("type ")} is a shell builtin")
+            command = command.removeprefix("type ")
+            foundInPath, pathOfCommand = checkPath(paths, command)
+            if command in commands and foundInPath:
+                print(f"{command} is {pathOfCommand}")
+                continue
+            elif command in commands:
+                print(f"{command} is a shell builtin")
                 continue
             else:
                 print(f"{command.removeprefix("type ")}: not found")
                 continue
-        if command == "exit 0":
-            exit(0)
+
         if "echo " in command:
             print(f"{command.removeprefix("echo ")}")
 
+        if command == "exit 0":
+            exit(0)
         else:
-            print(f"{command}: command not found")
+            if command in commands:
+                print(f"{command} is a command but is incorrectly formatted")
+            else:
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
